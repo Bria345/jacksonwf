f002781e2e51be9d51bf62345f7c0e3a3775ecc2
Bria345 <briana991@hotmail.com>
2026-06-04 18:21:16 +0100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-        let searchInputElement = document.querySelector("#search-form-input");
-      let cityElement = document.querySelector("#weather-city");
-      cityElement.innerHTML = searchInputElement.value;
-      
-       
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
+function displayWeatherCondition(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  
 
-let searchFormElement = document.getElementById("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit); 
+  let date = new Date(response.data.time * 1000);
+
+  if (cityElement) {
+    cityElement.innerHTML = response.data.city;
+  }
+  if (timeElement) {
+    timeElement.innerHTML = formatDate(date);
+  }
+  if (descriptionElement) {
+    descriptionElement.innerHTML = response.data.condition.description;
+  }
+  if (temperatureElement) {
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  }
+  if (humidityElement) {
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  }
+  if (windElement) {
+    windElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
+  }
+}
+
+function searchCity(city) {
+  let apiKey = "t3fabb4446203472a1b4b853oa5d0c30";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+if (searchFormElement) {
+  searchFormElement.addEventListener("submit", handleSearchFormSubmit);
+}
+
+// Loads a default city on refresh
+searchCity("Cayman Islands");
+
+                                                                                
